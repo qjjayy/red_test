d69cbca927fc9b3d19dca14f0b490fb91c7c95c9
仇俊杰 <jqiu1@xiaohongshu.com>
2019-04-04 10:44:11 +0800

    uuid-object-id

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(basedir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='auto-red-test',  # 包名称
-    version='0.1.5',  # 版本
+    version='0.1.6',  # 版本
     url='https://github.com/qjjayy/red_test',
     license='MIT',
     author='Junjie Qiu',
