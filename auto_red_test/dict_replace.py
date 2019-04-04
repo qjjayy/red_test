cfc1fa3f97ec383fd37099bcc793254bd15c420d
仇俊杰 <jqiu1@xiaohongshu.com>
2019-04-04 11:27:02 +0800

    replace-dict-none

--- auto_red_test/dict_replace.py
+++ auto_red_test/dict_replace.py
@@ -20,20 +20,30 @@ def _replace_item(attr_name, from_dict, to_dict):
 def _replace_list(attr_name, from_dict, to_dict):
     from_list = from_dict[attr_name]
     to_list = to_dict[attr_name]
+
+    if len(from_list) == 0:
+        to_dict[attr_name] = from_dict[attr_name]
+        return
+
     from_type = __get_value_type(from_list[0])
     to_type = __get_value_type(to_list[0])
     if from_type == to_type:
-        for i in range(LIST_COUNT):
-            if from_type == 'dict':
+        if from_type == 'dict':
+            for i in range(LIST_COUNT):
                 replace_param(None, from_type, from_list[i], to_list[i])
-            else:
-                to_dict[attr_name] = from_dict[attr_name]
+        else:
+            to_dict[attr_name] = from_dict[attr_name]
 
 
 def _replace_dict(attr_name, from_dict, to_dict):
     if attr_name:
         from_dict = from_dict[attr_name]
         to_dict = to_dict[attr_name]
+
+        if not from_dict:
+            to_dict[attr_name] = from_dict[attr_name]
+            return
+
     for key, value in to_dict.items():
         if key in from_dict.keys():
             from_type = __get_value_type(from_dict[key])
